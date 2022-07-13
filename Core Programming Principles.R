598e4d10f765e6afddf2b7979140a582d11e64f6
Andrey Cesar Vilachã <andreycesarrodrigues@hotmail.com>
2022-07-13 13:50:25 -0300

    Basics types in R

---
+++ Core Programming Principles.R
@@ -0,0 +1,13 @@
+#Use ctrl + Enter to execute a line of code
+#integer, use "L" to make a number a integer for R
+x <- 2L
+#Use typeof() to show the type of some data
+typeof(x)
+#Double
+y <- 2.5
+#Complex
+z <- 3 + 2i
+#character
+a <- "h"
+#Logical (TRUE/T,FALSE/F)
+q1 <- FALSE
