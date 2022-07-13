f6b8915916f54eec0c0211f0726dfb36a8d0281e
Andrey Cesar Vilachã <andreycesarrodrigues@hotmail.com>
2022-07-13 14:51:29 -0300

    Finished the first file

--- Core Programming Principles.R
+++ Core Programming Principles.R
@@ -11,3 +11,62 @@ z <- 3 + 2i
 a <- "h"
 #Logical (TRUE/T,FALSE/F)
 q1 <- FALSE
+
+A <- 10
+B <- 5
+
+C <- A + B
+#Using ctrl + Enter to print the object on console
+C
+
+var1 <- 2.5
+var2 <- 4
+
+result <- var1 / var2
+result
+
+#The square root function
+answer <- sqrt(var2)
+answer
+
+greeting <- "Hello"
+name <- "Bob"
+message <- paste(greeting, name)
+#Getting help 
+?paste
+help(sqrt)
+
+#Logical operators
+5 > 2
+6 < 4
+7 >= 1
+9 <= 3
+20 == 20
+30 != 50
+!T
+50 > 60 | 50 > 30
+50 > 60 & 50 > 30
+
+#While loop
+while(B < 10){
+  print("Hello")
+  B <- B +1
+}
+
+#For loop
+for(num in 1:5){
+  print(num)
+}
+
+#Removin a var
+rm(answer)
+#Creating random numbers from a normal distribution
+x <- rnorm(1)
+#Using if
+if(x <= -1){
+  answer <- "Less than -1"
+} else if(x >= 1){
+  answer <- "Greater than 1"
+} else{
+  answer <- "Is in between -1 and 1"
+}
