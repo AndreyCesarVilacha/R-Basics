5ef92b8596d6ea14fe848d8656eb15575a768d64
Andrey Cesar Vilachã <andreycesarrodrigues@hotmail.com>
2022-07-15 15:19:56 -0300

    Basics of Data Frame

---
+++ DataFrameEx.R
@@ -0,0 +1,37 @@
+#Importing data
+stats <- read.csv(file.choose())
+#Removing a Data frame
+rm(stats)
+
+#Getting the work directory
+#getwd()
+
+#Setting directory
+#setwd("C:\\Users\\userName\\nameFolderProject")
+
+#Getting numbers of rows in Data frame
+nrow(stats)
+
+#Getting numbers of columns in Data frame
+ncol(stats)
+
+#Getting the first 6 rows from Data frame
+head(stats)
+
+#Getting the 6 last rows from Data frame
+tail(stats)
+
+#Getting the structer of Data frame
+str(stats)
+
+#Getting a summary of Data frame
+summary(stats)
+
+#removing a column
+stats$Country.Name <- NULL
+
+#Filtering Data frame
+stats$Internet.users
+filterDF <- stats$Internet.users < 2
+stats[filterDF,]
+stats[stats$Country.Name == "Malta",]
