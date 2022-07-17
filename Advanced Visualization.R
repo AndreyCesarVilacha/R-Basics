3ed899a9c6be54337603ba9f283b30b2a6c041f9
Andrey Cesar Vilachã <andreycesarrodrigues@hotmail.com>
2022-07-17 15:01:45 -0300

    Fixing a comment

--- Advanced Visualization.R
+++ Advanced Visualization.R
@@ -1,7 +1,8 @@
 #Importing data
 movies  <- read.csv("P2-Movie-Ratings.csv")
-#Testing if the data has importing corrected
+#Checking if the import of data has successful or not
 head(movies)
 #Changing the columns names
 colnames(movies) <- c("Film", "Genre", "CriticRating", "AudienceRating", "BudgetMillions", "Year")
 head(movies)
+str(movies)
