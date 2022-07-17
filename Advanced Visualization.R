9899dfad09b1565801717c91e336f1f87d00a579
Andrey Cesar Vilachã <andreycesarrodrigues@hotmail.com>
2022-07-17 15:53:48 -0300

    Changing the xLabel

--- Advanced Visualization.R
+++ Advanced Visualization.R
@@ -15,3 +15,39 @@ summary(movies)
 #Converting years in factor
 movies$Year <- factor(movies$Year)
 summary(movies)
+
+#importing the library ggplot2
+library(ggplot2)
+
+#Using aes to map the data and geom to draw the graphic
+ggplot(data=movies, aes(x=CriticRating, y=AudienceRating, 
+                        colour=Genre, size = BudgetMillions)) + 
+  geom_point()
+
+#Plotting with layers
+p <-ggplot(data=movies, aes(x=CriticRating, y=AudienceRating, 
+                            colour=Genre, size = BudgetMillions))
+
+p + geom_point()
+
+#lines
+p + geom_line()
+
+#Multiple layers
+p + geom_point() + geom_line()
+
+#Overriding Aesthics
+q <- ggplot(data=movies, aes(x=CriticRating, y = AudienceRating, 
+                             colour=Genre, size = BudgetMillions))
+
+#Add geom layer
+q + geom_point()
+
+#Overriding aes
+q + geom_point(aes(size=CriticRating))
+
+
+#Overriding the label
+q + geom_point(aes(x=BudgetMillions)) + 
+  xlab("Budget Millions $$$")
+
