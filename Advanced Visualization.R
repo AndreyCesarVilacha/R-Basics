fbe48face9feaaee00a7b1b384705e70af4e03b7
Andrey Cesar Vilachã <andreycesarrodrigues@hotmail.com>
2022-07-17 15:08:45 -0300

    Converting year to fector

--- Advanced Visualization.R
+++ Advanced Visualization.R
@@ -1,8 +1,17 @@
 #Importing data
 movies  <- read.csv("P2-Movie-Ratings.csv")
+
 #Checking if the import of data has successful or not
 head(movies)
+
 #Changing the columns names
 colnames(movies) <- c("Film", "Genre", "CriticRating", "AudienceRating", "BudgetMillions", "Year")
 head(movies)
+
+#getting info from the data
 str(movies)
+summary(movies)
+
+#Converting years in factor
+movies$Year <- factor(movies$Year)
+summary(movies)
