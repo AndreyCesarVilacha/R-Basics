59c504af87e5813a932c335dec502267018c460b
Andrey Cesar Vilachã <andreycesarrodrigues@hotmail.com>
2022-07-17 18:04:01 -0300

    Theme, facet and coordinates

--- Advanced Visualization.R
+++ Advanced Visualization.R
@@ -51,3 +51,73 @@ q + geom_point(aes(size=CriticRating))
 q + geom_point(aes(x=BudgetMillions)) + 
   xlab("Budget Millions $$$")
 
+#Histragrams
+
+s <- ggplot(data=movies, aes(x=BudgetMillions))
+s + geom_histogram(binwidth = 10, aes(fill=Genre), colour="Black")
+
+#Density chart
+s + geom_density(aes(fill=Genre), position = "stack")
+
+#Layer Tips
+t <- ggplot(data=movies, aes(x=AudienceRating))
+t + geom_histogram(binwidth = 10,
+                   fill="White", colour="Blue")
+
+#Statistical Transformation
+u <- ggplot(data=movies, aes(x=CriticRating, y=AudienceRating, colour=Genre))
+
+u + geom_point() + geom_smooth(fill=NA)
+
+#Boxplots
+u <- ggplot(data=movies, aes(x=Genre, y=AudienceRating, colour=Genre))
+u + geom_boxplot(size=1.2) + geom_jitter()
+
+#Facets
+v <- ggplot(data=movies, aes(x=BudgetMillions))
+v + geom_histogram(binwidth = 10, aes(fill=Genre),
+                   colour="Black") +
+  facet_grid(Genre~., scale ="free")
+
+#Scatterplots
+w <- ggplot(data=movies, aes(x=CriticRating, y=AudienceRating,
+                             colour=Genre))
+w + geom_point(size=3) +
+  facet_grid(.~Genre)
+#facet 2
+w <- ggplot(data=movies, aes(x=CriticRating, y=AudienceRating,
+                             colour=Genre))
+w + geom_point(size=3) +
+  facet_grid(Genre~Year)
+
+#coordinates
+m <- ggplot(data=movies, aes(x=CriticRating, y=AudienceRating, 
+                             size=BudgetMillions, colour=Genre))
+m + geom_point() + xlim(50,100) + ylim(50,100)
+
+#Zooming instead
+n <- ggplot(data=movies, aes(x=BudgetMillions))
+n + geom_histogram(binwidth = 10,aes(fill=Genre), colour="Black") +
+  coord_cartesian(ylim = c(0,50))
+
+#Improving w
+w + geom_point(aes(size=BudgetMillions))+
+  geom_smooth()+
+  facet_grid(Genre~Year)+
+  coord_cartesian(ylim=c(0,100))
+
+#theme
+o <- ggplot(data=movies, aes(x=BudgetMillions))
+h <- o + geom_histogram(binwidth = 10, aes(fill=Genre), colour="Black")
+
+h + xlab("Money Axis") + ylab("Number of Movies") +
+  ggtitle("Movie Budget Distribution") +
+  theme(axis.title.x= element_text(colour="DarkGreen", size=30),
+        axis.title.y = element_text(colour="Red", size=30),
+        axis.text.x =  element_text(size = 20),
+        axis.text.y =  element_text(size = 20),
+        legend.title = element_text(size=30),
+        legend.text = element_text(size = 20),
+        legend.position = c(1,1),
+        legend.justification = c(1,1),
+        plot.title = element_text(colour = "DarkBlue", size = 10))
