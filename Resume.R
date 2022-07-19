43c521e52e03a2dd1e2d2521267534332efe68d9
Andrey Cesar Vilachã <andreycesarrodrigues@hotmail.com>
2022-07-19 16:38:36 -0300

    Data types, Variables, Operators

---
+++ Resume.R
@@ -0,0 +1,71 @@
+#------------------------Data Types----------------------------
+#Logical: TRUE,T,FALSE,F
+#Numerical: 23, 5.35
+#Integer: 23L, 55L
+#Complex: 3+2i
+#Character: 'a', "good"
+#Vector: c(5,3,4,2), c("Hello", "World")
+#List: list(10,2,5,3, "AA")
+#Matrix: matrix(c('a','b','c', 'd', 'e', 'f'), nrow = 2, ncol = 3, byrow = TRUE)
+#Array: array(c('green', 'yellow'), dim = c(3,2,4))
+#Factor: factor(c('green','green', 'yellow', 'red', 'yellow', 'red', 'red'))
+#Data frame: data.frame(
+#    gender = c("Male", "Male", "Female")
+#    height = c(153, 171.5, 165)
+#    age = c(42,38,26)
+#)
+
+#------------------------Variables----------------------------
+
+#Valid names for variables
+#   var_names2
+#   .var_name
+#   var.name3
+
+# Invalid names for variables
+#   var_name%
+#   2var_name
+#   .2var_name
+#   _var_name
+
+#Variable Assignment
+#   var.1 = c(0, 1, 2, 3)
+#   var.2 <- c("R", "Variable")
+#   c(T,F,2) -> var.3
+
+#Knowing the type of variable
+#   class(a)
+#   typeof(a)
+
+#Removing a variable
+#   rm(a)
+
+#------------------------Operators----------------------------
+
+#Add: +
+#Subtract: -
+#Multiplies: *
+#Divide: /
+#Reminder: %%
+#Division on vector: %/%
+#Exponential: ^
+
+#Greater than: >
+#Less than: <
+#Equality: ==
+#Less than or equal: <=
+#Greater than or equal: >=
+#If it is different: !=
+
+#AND: &&
+#OR: ||
+
+#Element Wise logical
+#AND: &
+#OR: |
+#NOT: !
+
+#Create a serie of elements: :
+#Operator to identify if element belong to a vector: %in%
+#Multiply matrix with its transpose: %*%
+
